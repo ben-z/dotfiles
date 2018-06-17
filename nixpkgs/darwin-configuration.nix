357faa1eab1b9dfcd424bbb2786881f37da29450
Ben Zhang <benzhangniu@gmail.com>
2018-06-16 21:37:02 -0400

    Improve vimrc

--- nixpkgs/darwin-configuration.nix
+++ nixpkgs/darwin-configuration.nix
@@ -77,9 +77,9 @@ in
       autoclose = pkgs.vimUtils.buildVimPluginFrom2Nix {
         name = "vim-autoclose-2018-06-16";
         src = pkgs.fetchgit {
-          url = "git://github.com/spf13/vim-autoclose";
-          rev = "c098b4f45da6232e66c14898ff6b197f360a29cc";
-          sha256 = "0yxv44v2axbi2rw68xa5r2qaig8dskh9mhqrm13iqjpg33d5gnyc";
+          url = "git://github.com/ben-z/vim-autoclose";
+          rev = "22ba641a337000346a77c8bb99bfc2248657cd80";
+          sha256 = "009yr0gaz3hwgc4904c4bjarg7gag7kx4b3pis2syd8xdjrqap85";
         };
         dependencies = [];
       };
@@ -93,6 +93,118 @@ in
       let g:ycm_confirm_extra_conf = 0
       let g:airline_theme='solarized'
       let g:airline_solarized_bg='dark'
+
+      " Many options below are taken from https://github.com/spf13/spf13-vim/blob/3.0/.vimrc
+
+      " Use system clipboard {
+        if has('clipboard')
+          if has('unnamedplus')  " When possible use + register for copy-paste
+            set clipboard=unnamed,unnamedplus
+          else                   " On mac and Windows, use * register for copy-paste
+            set clipboard=unnamed
+          endif
+        endif
+      " }
+
+      " Allow for cursor beyond last character {
+        set virtualedit=onemore
+      " }
+
+
+      " Restore cursor position {
+        function! ResCur()
+          if line("'\"") <= line("$")
+            normal! g`"
+            return 1
+          endif
+        endfunction
+
+        augroup resCur
+          autocmd!
+          autocmd BufWinEnter * call ResCur()
+        augroup END
+      " }
+
+      " Backup and persistent undo {
+        set backup
+        if has('persistent_undo')
+            set undofile
+            set undolevels=1000         " Maximum number of changes that can be undone
+            set undoreload=10000        " Maximum number lines to save for undo on a buffer reload
+        endif
+      " }
+
+      " Strip whitespace {
+        function! StripTrailingWhitespace()
+          " Preparation: save last search, and cursor position.
+          let _s=@/
+          let l = line(".")
+          let c = col(".")
+          " do the business:
+          %s/\s\+$//e
+          " clean up: restore previous search history, and cursor position
+          let @/=_s
+          call cursor(l, c)
+        endfunction
+
+        " Automatically strip trailing whitespace for the following file types
+        autocmd FileType c,cpp,java,go,php,javascript,puppet,python,rust,twig,xml,yml,perl,sql,nix autocmd BufWritePre <buffer> call StripTrailingWhitespace()
+      " }
+
+      " Wrapped lines goes down/up to next row, rather than next line in file {
+        noremap j gj
+        noremap k gk
+      " }
+
+      " Map commonly mistyped keys {
+        if has("user_commands")
+          command! -bang -nargs=* -complete=file E e<bang> <args>
+          command! -bang -nargs=* -complete=file W w<bang> <args>
+          command! -bang -nargs=* -complete=file Wq wq<bang> <args>
+          command! -bang -nargs=* -complete=file WQ wq<bang> <args>
+          command! -bang Wa wa<bang>
+          command! -bang WA wa<bang>
+          command! -bang Q q<bang>
+          command! -bang QA qa<bang>
+          command! -bang Qa qa<bang>
+        endif
+      " }
+
+      " Some useful shortcuts {
+        " For when you forget to sudo.. Really Write the file.
+        cmap w!! w !sudo tee % >/dev/null
+
+        " Change Working Directory to that of the current file
+        cmap cwd lcd %:p:h
+        cmap cd. lcd %:p:h
+      " }
+
+      " Fugitive {
+        nnoremap <silent> <leader>gs :Gstatus<CR>
+        nnoremap <silent> <leader>gd :Gdiff<CR>
+        nnoremap <silent> <leader>gc :Gcommit<CR>
+        nnoremap <silent> <leader>gb :Gblame<CR>
+        nnoremap <silent> <leader>gl :Glog<CR>
+        nnoremap <silent> <leader>gp :Git push<CR>
+        nnoremap <silent> <leader>gr :Gread<CR>
+        nnoremap <silent> <leader>gw :Gwrite<CR>
+        nnoremap <silent> <leader>ge :Gedit<CR>
+        " Mnemonic _i_nteractive
+        nnoremap <silent> <leader>gi :Git add -p %<CR>
+        nnoremap <silent> <leader>gg :SignifyToggle<CR>
+      " }
+
+      " UndoTree {
+        nnoremap <Leader>u :UndotreeToggle<CR>
+        " If undotree is opened, it is likely one wants to interact with it.
+        let g:undotree_SetFocusWhenToggle=1
+      " }
+
+      " AutoCloseTag {
+        " Make it so AutoCloseTag works for xml and xhtml files as well
+        au FileType xhtml,xml ru ftplugin/html/autoclosetag.vim
+        nmap <Leader>ac <Plug>ToggleAutoCloseMappings
+      " }
       '';
   };
 }
