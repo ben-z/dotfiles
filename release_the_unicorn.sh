22437b47cd5aa4fe9bdd04027c4aa3a2a9dd3bc0
Ben Zhang <benzhangniu@gmail.com>
2018-07-16 20:23:51 -0400

    WIP setup script

---
+++ release_the_unicorn.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Release the Unicorn!
+
+# 1. Install nix
+# 2. Install nix-darwin
+# 3. Clone dotfiles
+# 4. Link dotfiles
+# 5. Install applications
+#   5.1 Little Snitch
+#   5.2 Bartender
+#   5.3 Paste
+#   5.4 Amethyst
+#   5.5 iTerm - apply solarized theme from this repo
+#   5.6 1Password
+#   5.7 Hammerspoon - configuration in this repo
+#   5.7 Chrome
+
+
+if ! [ -x "$(command -v nix-env)" ]; then
+  echo 'Nix has not been installed! Install it with `curl https://nixos.org/nix/install | sh`' >&2
+  exit 1
+fi
+
+# TODO
